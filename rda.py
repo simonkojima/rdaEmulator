c6b99f409da6190a63c461149397c78db3f77a1a
Simon Kojima <simon.kojima@outlook.com>
2022-07-08 12:55:19 +0200

    numpy to np

--- rda.py
+++ rda.py
@@ -1,7 +1,7 @@
 import argparse
 import socket
 import time
-import numpy
+import numpy as np
 import mne
 
 """
@@ -23,13 +23,13 @@ MARKER_CHANNEL = 0
 DEFAULT_CH_RESOLUTION = 0.1
 NUMBER_OF_DATA_POINTS = 40
 ENABLE_REALTIME = True
-SLEEP_COEF = 0.88
+SLEEP_COEF = 0.9
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 51244  # Port to listen on (non-privileged ports are > 1023)
 
 BV_RECORDER_ID = [-114, 69, 88, 67, -106, -55, -122, 76, -81, 74, -104, -69, -10, -55, 20, 80]
-ids = numpy.array(BV_RECORDER_ID, dtype=numpy.int8)
+ids = np.array(BV_RECORDER_ID, dtype=np.int8)
 BV_RECORDER_ID_BYTE = ids.tobytes()
 
 #----------------------------------------------------------------------------------
@@ -39,13 +39,13 @@ def gen_header(id, msgsize, msgtype):
     header = bytes()
 
     #for m in range(0, 4):
-    #    tmp = numpy.array(id[m]).astype(numpy.int32)
+    #    tmp = np.array(id[m]).astype(np.int32)
     #    header += tmp.tobytes()
 
     header += BV_RECORDER_ID_BYTE
     
-    header += numpy.array(msgsize).astype(numpy.uint32).tobytes()
-    header += numpy.array(msgtype).astype(numpy.uint32).tobytes()
+    header += np.array(msgsize).astype(np.uint32).tobytes()
+    header += np.array(msgtype).astype(np.uint32).tobytes()
 
     return header
 
@@ -53,7 +53,7 @@ def string2byte(string_array, format='ascii'):
     r = bytes()
     for string in string_array:
         r += bytes(string, format)
-        r += numpy.array([0]).astype(numpy.int8).tobytes()
+        r += np.array([0]).astype(np.int8).tobytes()
     return r
 
 def gen_data_packets(eeg, markers, block, idx):
@@ -67,63 +67,63 @@ def gen_data_packets(eeg, markers, block, idx):
             eeg_send = eeg[:, idx:]
 
             num_zeros = data_points - eeg_send.shape[1]
-            zero_padding = numpy.zeros((eeg_send.shape[0], num_zeros))
-            eeg_send = numpy.concatenate((eeg_send, zero_padding), axis=1)
+            zero_padding = np.zeros((eeg_send.shape[0], num_zeros))
+            eeg_send = np.concatenate((eeg_send, zero_padding), axis=1)
 
             markers_send = markers[idx:]
-            zero_padding = numpy.zeros(num_zeros)
-            markers_send = numpy.concatenate((markers_send, zero_padding))
-            markers_count = numpy.count_nonzero(markers_send)
+            zero_padding = np.zeros(num_zeros)
+            markers_send = np.concatenate((markers_send, zero_padding))
+            markers_count = np.count_nonzero(markers_send)
 
             idx = -1 # Last block
 
     else:
         eeg_send = eeg[:, idx:idx+data_points]
         markers_send = markers[idx:idx+data_points]
-        markers_count = numpy.count_nonzero(markers_send)
+        markers_count = np.count_nonzero(markers_send)
         idx += data_points
 
     block = block + 1
 
     n_row, n_column = eeg_send.shape
 
-    eeg_send = numpy.reshape(eeg_send,n_row*n_column, order='F')
+    eeg_send = np.reshape(eeg_send,n_row*n_column, order='F')
 
     body = bytes()
 
-    tmp = numpy.array(block).astype(numpy.uint32)
+    tmp = np.array(block).astype(np.uint32)
     body += tmp.tobytes()
-    tmp = numpy.array(data_points).astype(numpy.uint32)
+    tmp = np.array(data_points).astype(np.uint32)
     body += tmp.tobytes()
-    tmp = numpy.array(markers_count).astype(numpy.uint32)
+    tmp = np.array(markers_count).astype(np.uint32)
     body += tmp.tobytes()
     for sample_point in eeg_send:
-        tmp = numpy.array(sample_point).astype(numpy.float32)
+        tmp = np.array(sample_point).astype(np.float32)
         body += tmp.tobytes()
 
     if markers_count > 0:
-            tmp = numpy.argwhere(markers_send)
-            markers_list_send = numpy.concatenate((tmp, markers_send[tmp]), axis=1)
+            tmp = np.argwhere(markers_send)
+            markers_list_send = np.concatenate((tmp, markers_send[tmp]), axis=1)
             marker_block = bytes()
             for marker in markers_list_send:
                 marker_block_single = bytes()
                 position = marker[0]
-                tmp = numpy.array(position).astype(numpy.uint32)
+                tmp = np.array(position).astype(np.uint32)
                 marker_block_single += tmp.tobytes()
 
                 points = NUMBER_OF_MARKER_POINTS
-                tmp = numpy.array(points).astype(numpy.uint32)
+                tmp = np.array(points).astype(np.uint32)
                 marker_block_single += tmp.tobytes()
 
                 channel = MARKER_CHANNEL
-                tmp = numpy.array(points).astype(numpy.int32)
+                tmp = np.array(points).astype(np.int32)
                 marker_block_single += tmp.tobytes()
 
                 type_desc = string2byte(["S"+str(marker[1]), "S"+str(marker[1])])
                 marker_block_single += type_desc
 
                 marker_size = len(marker_block_single)+4
-                tmp = numpy.array(marker_size).astype(numpy.uint32)
+                tmp = np.array(marker_size).astype(np.uint32)
 
                 marker_block_single = tmp.tobytes() + marker_block_single
 
@@ -141,18 +141,18 @@ def gen_data_packets(eeg, markers, block, idx):
 def gen_zero_packets(n_ch, data_points,block):
     block = block + 1
 
-    data = numpy.zeros(n_ch*data_points)
+    data = np.zeros(n_ch*data_points)
 
     body = bytes()
 
-    tmp = numpy.array(block).astype(numpy.uint32)
+    tmp = np.array(block).astype(np.uint32)
     body += tmp.tobytes()
-    tmp = numpy.array(data_points).astype(numpy.uint32)
+    tmp = np.array(data_points).astype(np.uint32)
     body += tmp.tobytes()
-    tmp = numpy.array([0]).astype(numpy.uint32)
+    tmp = np.array([0]).astype(np.uint32)
     body += tmp.tobytes()
     for sample_point in data:
-        tmp = numpy.array(sample_point).astype(numpy.float32)
+        tmp = np.array(sample_point).astype(np.float32)
         body += tmp.tobytes()
 
     msgtype = 4
@@ -178,7 +178,7 @@ events = mne.events_from_annotations(raw)
 events = events[0]
 events = events[1:]
 
-markers = numpy.zeros((eeg.shape[1])).astype(numpy.int8)
+markers = np.zeros((eeg.shape[1])).astype(np.int8)
 for event in events:
     markers[event[0]] = event[2]
 
@@ -201,14 +201,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         body = bytes()
 
-        tmp = numpy.array(raw.info['nchan']).astype(numpy.int32)
+        tmp = np.array(raw.info['nchan']).astype(np.int32)
         body += tmp.tobytes()
 
-        tmp = numpy.array(1000000/raw.info['sfreq']).astype(numpy.float64)
+        tmp = np.array(1000000/raw.info['sfreq']).astype(np.float64)
         body += tmp.tobytes()
 
         for m in range(raw.info['nchan']):
-            tmp = numpy.array(resolution[m]).astype(numpy.float64)
+            tmp = np.array(resolution[m]).astype(np.float64)
             body += tmp.tobytes()
 
         tmp = bytes()
